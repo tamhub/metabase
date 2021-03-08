5e52d835b40681444ce124c3cc51e528575d544e
AhmadWork <a7madswe@gmail.com>
2021-03-08 08:58:19 +0300

    Update TitleAndDescription component

--- frontend/src/metabase/components/TitleAndDescription.jsx
+++ frontend/src/metabase/components/TitleAndDescription.jsx
@@ -5,6 +5,7 @@ import pure from "recompose/pure";
 
 import Icon from "metabase/components/Icon";
 import Tooltip from "metabase/components/Tooltip";
+import colors from "metabase/lib/colors";
 
 type Attributes = {
   title: string,
@@ -12,13 +13,9 @@ type Attributes = {
   className?: string,
 };
 const TitleAndDescription = ({ title, description, className }: Attributes) => (
-  <div className={cx("flex align-center", className)}>
-    <h2 className="h2 mr1 text-wrap">{title}</h2>
-    {description && (
-      <Tooltip tooltip={description} maxWidth={"22em"}>
-        <Icon name="info" className="mx1" />
-      </Tooltip>
-    )}
+  <div className={cx("flex justify-content-center align-items-center", className)}>
+    <h2 className="h2 text-wrap text-center" style={{ color: colors.accent5}}>{title}</h2>
+     <span>{description}</span>
   </div>
 );
 
